2709697332639668f6ec8b724cd1b86c12a7707f
Ayush Raj <ayussshhraj@gmail.com>
2025-01-18 00:51:39 +0530

    Data types in JS

---
+++ 04_JSFoundation/02_datatype.js
@@ -0,0 +1,35 @@
+console.log("helo")
+//console.log("world") this is single line comment
+/*
+this is multi line comment 
+
+        TYPES OF DATATYPE
+         
+    String
+    Number
+    Boolean
+    Bigint
+
+    Undefined
+    Null
+
+    Object
+
+    Symbol
+
+*/
+
+//var score =102    //old type
+
+let score = 102  // mordern 
+let name = "Ayush Raj"
+let isLoggedin = false
+
+let getScore = score
+
+console.log(getScore)
+
+//objecs
+
+let teaType = ["leamon tea", "orange tea", "oolong tea"];
+let user = {firstname: "Ayush" ,  lastname: "Raj"};
