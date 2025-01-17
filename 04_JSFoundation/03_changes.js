3e9862b2921a39e22ad8adec0d60cb56628367c4
Ayush Raj <ayussshhraj@gmail.com>
2025-01-18 01:09:21 +0530

    changes in JS

---
+++ 04_JSFoundation/03_changes.js
@@ -0,0 +1,11 @@
+let gameName = "Spiderman";
+
+gameName = "GTA-V";
+
+console.log(gameName);
+
+const username = "Ayussshh_07";   //const keyword is used to declare constants . cannot be reassigned after its initial assignment
+
+//username = "Ayush";   //This will throw a TypeError
+
+console.log(username);
