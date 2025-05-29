8b508dcc77a3a921d31bb3c5dcc577492c0f2096
Ayush Raj <ayussshhraj@gmail.com>
2025-05-29 19:58:49 +0530

    Functions

---
+++ 04_JSFoundation/part5/functions.js
@@ -0,0 +1,17 @@
+/*
+1. Write a funnction named 'makeTea' that takes one parameter, 'typeOfTea', and returns a string like '"Making green tea"' when called with '"green tea"' . 
+    store the result in a variable named 'teaOrder'
+*/
+
+function makeTea(typeOfTea) {
+    return `Making ${typeOfTea}`;
+    console.log(test);
+}
+let teaOrder = makeTea("Lemon tea");
+console.log(teaOrder)
+
+/*
+2. Creat a function named 'orderTea' that takes one parameter, 'teaType' . Inside this function, create another function named 'confirmOrder' that returns a message like '"Order confirmed for chai"'.
+Call'confirmOrder' from within 'orderTea' and return the result.
+*/
+
