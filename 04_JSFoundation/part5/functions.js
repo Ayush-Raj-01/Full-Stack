208285fe25d2a65bd07ed9fa99519d0e520d5976
Ayush Raj <ayussshhraj@gmail.com>
2025-05-29 21:02:37 +0530

    Functions

--- 04_JSFoundation/part5/functions.js
+++ 04_JSFoundation/part5/functions.js
@@ -15,3 +15,58 @@ console.log(teaOrder)
 Call'confirmOrder' from within 'orderTea' and return the result.
 */
 
+function orderTea(teaType){
+    function confirmOrder(){
+        return 'order confirmed for chai'
+    }
+    return confirmOrder()
+}
+
+let orderConfirmation = orderTea("CHAI")
+console.log(orderConfirmation)
+
+/*
+3. Write an arrow function named 'catcutateTotal' that takes two parameters: 'price' and 'quantity'. The function should return the totat cost by muttiptying the 'price' and 'quantity' . 
+Store the result in a variable named 'totatCost'.
+*/
+
+// const catcutateTotal = (price , quantity) => {
+//     return price * quantity
+// }
+// we can write the same in this way also but by removing brackets and return and bu writing things in one line 
+const catcutateTotal = (price , quantity) => price * quantity
+
+let totatCost = catcutateTotal(499 , 100)
+console.log(totatCost)
+
+/*
+4. Write a function named 'processTeaOrder' that takes another functon , 'makeTea',  as a parameter and calls it with the argument '"rarl grey"'.
+Return the result of caling 'makeTea'
+*/
+
+function makeTea(typeOfTea){
+    return `maketea: ${typeOfTea}`
+}
+
+function processTeaOrder(teaFunction){
+    return teaFunction("earlgrey")
+}
+
+
+let order = processTeaOrder (makeTea)
+    console.log(order);
+    
+/*
+5. Write a function named 'createTeaMaker' that returns another function. The returned function should take one parameter, 'teaType', and return a message like '"Making green tea"'
+Store the returned function in a variable named 'teaMaker' and call it with '"green tea"'
+*/
+
+function createTeaMaker(){
+    return function(teaType){
+        return `Made ${teaType}`
+    }
+}
+
+let teaMaker = createTeaMaker()
+let result = teaMaker("green tea")
+console.log(result)
