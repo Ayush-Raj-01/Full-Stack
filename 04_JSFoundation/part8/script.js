63b806e17097eda5e552833f9a07a3278de7d6c0
Ayush Raj <ayussshhraj@gmail.com>
2025-06-02 23:19:35 +0530

    script

---
+++ 04_JSFoundation/part8/script.js
@@ -0,0 +1,13 @@
+// example - 1
+
+document.getElementById('changeTextButton').addEventListener("click" , function () {
+    let para = document.getElementById('myParagraph')
+    para.textContent = "THE PARAGRAPH IS CHANGED"
+})
+
+
+// example - 2
+
+document.getElementById("highlightFirstCity").addEventListener("click" , function () {
+    document.getElementById("citiesList")
+})
