da57c95a6febce28177f3fd525432691e8cc69fd
Ayush Raj <ayussshhraj@gmail.com>
2025-06-04 23:27:58 +0530

    script

--- 04_JSFoundation/part8/script.js
+++ 04_JSFoundation/part8/script.js
@@ -9,5 +9,38 @@ document.getElementById('changeTextButton').addEventListener("click" , function 
 // example - 2
 
 document.getElementById("highlightFirstCity").addEventListener("click" , function () {
-    document.getElementById("citiesList")
+    let citiesList = document.getElementById("citiesList")
+    citiesList.firstElementChild.classList.add("highlight")
+})
+
+// example - 3
+
+document.getElementById("changeOrder").addEventListener("click" , function () {
+    let coffeeType =  document.getElementById("coffeeType")
+    coffeeType.textContent = "Espresso"
+})
+
+
+// example - 4
+
+document.getElementById("addNewItem").addEventListener("click" , function () {
+
+    let newItem = document.createElement("li")
+    newItem.textContent = "Egg"
+    document.getElementById("shoppingList").append(newItem)
+})
+
+
+// example - 5
+
+document.getElementById("removeLastTask").addEventListener("click" , function () {
+    let taskList = document.getElementById("taskList")
+    taskList.lastElementChild.remove()
+})
+
+
+// example - 6
+
+document.getElementById("clickMeButton").addEventListener("click" , function () {
+    alert("DOM Example")
 })
