3aee97585a4392b368a9a4c1aa0af2039cb4200c
Ayush Raj <ayussshhraj@gmail.com>
2025-06-06 00:12:49 +0530

    Import file of Maths operation by Common js

---
+++ 04_JSFoundation/part9_adv/appC.js
@@ -0,0 +1,9 @@
+// CONNECTES FROM "mathOperationsC.js"
+
+// IMPORT FILE 
+
+const mathOperations = require("./mathOperationsC.js")
+
+console.log(mathOperations.add( 3 , 4 ))
+console.log(mathOperations.subtract( 3 , 5 ))
+console.log(mathOperations.multiply( 3 , 4 ))
