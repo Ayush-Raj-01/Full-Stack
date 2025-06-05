312349e3947d48526b722ab202125ad93363821b
Ayush Raj <ayussshhraj@gmail.com>
2025-06-06 00:18:05 +0530

    Prototypal Inheritance in JS

--- 04_JSFoundation/part9_adv/PrototypalInheritance.js
+++ 04_JSFoundation/part9_adv/PrototypalInheritance.js
@@ -1,4 +1,4 @@
-        // prototypal inheritance is just inheritance being done in the prototypes
+        // prototypal inheritance is just inheritance being done in the prototypes (Objects inherit directly from other objects)
 
 
 function Person(name) {
