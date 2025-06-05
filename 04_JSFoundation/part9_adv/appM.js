70ec1603afb0928bc46bec12554695f069971177
Ayush Raj <ayussshhraj@gmail.com>
2025-06-06 00:13:14 +0530

    Import file of Maths operation by Module js

---
+++ 04_JSFoundation/part9_adv/appM.js
@@ -0,0 +1,14 @@
+// CONNECTES FROM "mathOperationsM.js"
+
+// MODULE IMPORT FILE 
+
+// default import
+import multiply from "./mathOperationsM.js";
+
+
+// named import
+import { add , subtract} from "./mathOperationsM.js"
+
+
+console.log(multiply(2,2))
+console.log(subtract(2,2))
