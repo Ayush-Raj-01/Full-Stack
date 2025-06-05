7f4040f74ffbb521bf9fe1fda23eca2573cb3ada
Ayush Raj <ayussshhraj@gmail.com>
2025-06-05 21:18:41 +0530

    Asynchronous Javascript

---
+++ 04_JSFoundation/part9_adv/explore1.js
@@ -0,0 +1,17 @@
+        //  Asynchronous Javascript == 
+
+console.log("Ayush Raj")
+
+
+function sayHello () {
+    console.log("I would like to say Hello")
+}
+
+setTimeout(() => {
+    sayHello()
+} , 4000)
+
+
+for (let index = 0; index < 5; index++) {
+    console.log(index)    
+}
